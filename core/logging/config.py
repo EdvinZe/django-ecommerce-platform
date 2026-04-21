853e684c4d16ccd6008b9fb9f9bf1226dab9b7bb
Edvin <crypted946@email.com>
2026-04-21 22:18:32 +0300

    fix: logging for production

--- core/logging/config.py
+++ core/logging/config.py
@@ -1,52 +1,47 @@
 from pathlib import Path
+from backend.settings import DEBUG
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
+if DEBUG:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
 
-    "formatters": {
-        "color": {
-            "()": "core.logging.formatters.ColorFormatter",
-            "format": "[{levelname}] {asctime} {name}: {message}",
-            "style": "{",
+        "formatters": {
+            "color": {
+                "()": "core.logging.formatters.ColorFormatter",
+                "format": "[{levelname}] {asctime} {name}: {message}",
+                "style": "{",
+            },
         },
-        "simple": {
-            "format": "[{levelname}] {asctime} {name}: {message}",
-            "style": "{",
-        },
-    },
 
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "color",
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "color",
+            },
         },
-        "file": {
-            "class": "logging.FileHandler",
-            "filename": BASE_DIR / "logs/app.log",
-            "formatter": "simple",
+
+        "root": {
+            "handlers": ["console"],
             "level": "INFO",
         },
-        "error_file": {
-            "class": "logging.FileHandler",
-            "filename": BASE_DIR / "logs/errors.log",
-            "formatter": "simple",
-            "level": "ERROR",
+    }
+
+else:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+            },
         },
-    },
 
-    "root": {
-        "handlers": ["console", "file", "error_file"],
-        "level": "INFO",
-    },
-
-    "loggers": {
-        "django.utils.autoreload": {
+        "root": {
             "handlers": ["console"],
-            "level": "WARNING",
-            "propagate": False,
+            "level": "INFO",
         },
-    },
-}
+    }
